1557adb4b111d8df5ac5d9fa98a42b6f5e428c61
himanshu-mishra22 <122457025+himanshu-mishra22@users.noreply.github.com>
2024-06-09 21:10:37 -0700

    Add files via upload

---
+++ digital-clock/digital_clock.js
@@ -0,0 +1,12 @@
+function updateClock(){
+    const now = new Date();
+    const hrs = now.getHours().toString().padStart(2,0);
+    const mins = now.getMinutes().toString().padStart(2,0);;
+    const secs = now.getSeconds().toString().padStart(2,0);;
+    const timeString = `${hrs}:${mins}:${secs}`;
+    document.getElementById("clock").textContent = timeString;
+
+}
+
+
+setInterval(updateClock, 1000);
